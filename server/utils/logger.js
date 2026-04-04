809e3139dfa7aead046d88755291958616e274ef
jjjun02 <jjjun0622@gmail.com>
2026-04-04 16:57:06 +0900

    commit

---
+++ server/utils/logger.js
@@ -0,0 +1,33 @@
+const winston = require('winston');
+const winstonDaily = require('winston-daily-rotate-file');
+const path = require('path');
+
+const logDir = path.join(__dirname, '../logs');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    winston.format.json()
+  ),
+  transports: [
+    new winstonDaily({
+      level: 'info',
+      datePattern: 'YYYY-MM-DD',
+      dirname: logDir,
+      filename: `%DATE%.log`,
+      maxFiles: '30d',
+      zippedArchive: true,
+    }),
+    new winstonDaily({
+      level: 'error',
+      datePattern: 'YYYY-MM-DD',
+      dirname: logDir + '/error',
+      filename: `%DATE%.error.log`,
+      maxFiles: '30d',
+      zippedArchive: true,
+    }),
+  ],
+});
+
+module.exports = logger;
